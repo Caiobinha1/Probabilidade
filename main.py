8d3759d81eb2bf71482f8b89639b6447a51547e7
Caio Missfeld Carlos <104043844+Caiobinha1@users.noreply.github.com>
2023-10-25 13:27:26 -0300

    Simplificacao, ainda falta fazer as condicoes de cada scatter

--- main.py
+++ main.py
@@ -1,59 +1,28 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def printhistogramas1(imc,imcF,imcM):
-    plt.hist(imc, bins=100, edgecolor='black')  # `bins` define o número de barras no histograma
-    plt.xlabel('IMC')  
+def plot_histogram(imc, title):
+    plt.hist(imc, bins=100, edgecolor='black')
+    plt.xlabel('IMC')
     plt.xlim(0, 100)
-    plt.ylabel('Frequência')  
-    plt.title('Histograma do IMC total')  
-    plt.show()  
+    plt.ylabel('Frequência')
+    plt.title(title)
+    plt.show()
 
-    plt.hist(imcF, bins=100, edgecolor='black')  
-    plt.xlim(0, 100)
-    plt.ylabel('Frequência')  
-    plt.title('Histograma do IMC Feminino')  
-    plt.show()  
-
-    plt.hist(imcM, bins=100, edgecolor='black')  
-    plt.xlabel('IMC')  
-    plt.xlim(0, 100)
-    plt.ylabel('Frequência')  
-    plt.title('Histograma do IMC Masculino')  
-    plt.show() 
-
-def printhistogramas2(imcp,imca):
-    plt.hist(imcp, bins=100, edgecolor='black') 
-    plt.xlabel('IMC')  
-    plt.xlim(0, 100)
-    plt.ylabel('Frequência')  
-    plt.title('Histograma do IMC com presenca de doenca cardiovascular')  
-    plt.show()  
-
-    plt.hist(imca, bins=100, edgecolor='black')  
-    plt.xlabel('IMC')  
-    plt.xlim(0, 100)
-    plt.ylabel('Frequência')  
-    plt.title('Histograma do IMC com ausencia de doenca cardiovascular')  
-    plt.show() 
-    
-def graficodispersao(dado1,dado2):
-    plt.scatter(dado1,dado2,alpha=0.5,marker='o')        #olhar pra fazer esse grafico logaritimo
+def plot_scatter(dado1, dado2, nome):
+    plt.scatter(dado1, dado2, alpha=0.5, marker='o')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Pressao diastolica (escala log)')
     plt.ylabel('Pressao sistolica (escala log)')
-    plt.title('Grafico de dispersao" Diastolica x sistolica')
+    plt.title(f'Grafico de dispersao" Diastolica x sistolica {nome}')
     plt.show()
-age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio, imc, imcF, imcM, imcp, imca = ([] for _ in range(17))
 
+age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio, imc, imcF, imcM, imcp, imca = ([] for _ in range(17))
 
 data = np.genfromtxt('cardiovascular_data.csv', delimiter=';', skip_header=1)
 
-
-age = (data[:, 0] / 365).astype(int)  # Transforma dias em anos
+age = (data[:, 0] / 365).astype(int)
 gender = data[:, 1].astype(int)
 height = data[:, 2].astype(int)
 weight = data[:, 3].astype(float)
@@ -66,31 +35,29 @@ alco = data[:, 9].astype(int)
 active = data[:, 10].astype(int)
 cardio = data[:, 11].astype(int)
 
-
 mediaaltura = np.mean(height)
 mediapeso = np.mean(weight)
 
-# Verifica e imprime os valores fora do normal para altura e peso
 for i, height_value in enumerate(height):
-    if height_value / mediaaltura > 2 or height_value / mediaaltura < 0.2:
-        print(height_value, "Está fora do normal para altura.") # altura fora do normal
-    if gender[i]==1:
+    if height_value / mediaaltura > 1.8 or height_value / mediaaltura < 0.5:
+        print(height_value, "Está fora do normal para altura.")
+    if gender[i] == 1:
         imcF.append((weight[i]) / ((height_value/100) ** 2))
     else:
         imcM.append((weight[i]) / ((height_value/100) ** 2))
-    imc.append((weight[i]) / ((height_value/100) ** 2))            #imc de cada pessoa
-
-
+    imc.append((weight[i]) / ((height_value/100) ** 2))
 
 for i, weight_value in enumerate(weight):
-    if weight_value / mediapeso > 2 or weight_value / mediapeso < 0.2:
-        print(weight_value, "Está fora do normal para peso.")   # peso fora do normal
-    if cardio[i]==0:
+    if weight_value / mediapeso > 1.8 or weight_value / mediapeso < 0.5:
+        print(weight_value, "Está fora do normal para peso.")
+    if cardio[i] == 0:
         imca.append((weight_value) / ((height[i]/100) ** 2))
     else:
         imcp.append((weight_value) / ((height[i]/100) ** 2))
-    
 
-#printhistogramas1(imc,imcF,imcM)
-#printhistogramas2(imcp,imca)
-graficodispersao(ap_lo,ap_hi)
+plot_histogram(imc, 'Histograma do IMC total')
+plot_histogram(imcF, 'Histograma do IMC Feminino')
+plot_histogram(imcM, 'Histograma do IMC Masculino')
+plot_histogram(imcp, 'Histograma do IMC com doenca cardiovascular')
+plot_histogram(imca, 'Histograma do IMC sem doenca cardiovascular')
+plot_scatter(ap_lo, ap_hi, 'Grafico de dispersao: Diastolica x Sistolica Total')    
