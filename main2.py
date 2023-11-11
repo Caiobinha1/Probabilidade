af85529c0ba05ea53916f60945003ed396fa835b
Caio Missfeld Carlos <104043844+Caiobinha1@users.noreply.github.com>
2023-11-10 22:28:10 -0300

    commit final

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-    
+
 def calculate_statistics(data_file, sample_size, num_samples=100000):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(data_file, delimiter=';')
@@ -12,8 +12,19 @@ def calculate_statistics(data_file, sample_size, num_samples=100000):
     sample_variances = []
     
     # Perform random sampling with replacement
-    for _ in range(num_samples):
+    for i in range(num_samples):
         sample = df.sample(n=sample_size, replace=True)
+        
+        if i == 0:  # Plot histogram for the first sample only
+            for column in sample.columns:
+                plt.figure()
+                plt.hist(sample[column], bins=15, density=True, alpha=0.6)
+                plt.xlabel(column)
+                plt.ylabel('Density')
+                plt.title(f'Histogram of {column} in the First Sample')
+                plt.show()
+        sample['ap_hi'] = np.clip(sample['ap_hi'], None, 300)
+        sample['ap_lo'] = np.clip(sample['ap_lo'], None, 300)
         
         # Calculate mean, std dev, and variance for the sample
         sample_mean = sample.mean()
@@ -30,16 +41,29 @@ def calculate_statistics(data_file, sample_size, num_samples=100000):
     std_devs_df = pd.DataFrame(sample_std_devs)
     variances_df = pd.DataFrame(sample_variances)
 
-    overall_mean = means_df.mean()
-    overall_std_dev = std_devs_df.mean()
-    overall_variance = variances_df.mean()
+    overall_mean= means_df.mean()
+    overall_mean_std = means_df.std()
     
-    return means_df, std_devs_df, variances_df,overall_mean,overall_std_dev,overall_variance
+    overall_std_dev = std_devs_df.mean()
+    overall_std_std =std_devs_df.std()
+
+    overall_variance = variances_df.mean()
+    overall_variance_std = variances_df.std()
+
+    for column in means_df.columns:
+        plt.figure()
+        plt.hist(means_df[column], bins=25, density=True, alpha=0.6, color='b')
+        plt.xlabel(column)
+        plt.ylabel('Density')
+        plt.title(f'Histogram of {column} in Sample Means (100k samples)')
+        plt.show()
+    
+    return means_df, std_devs_df, variances_df,overall_mean,overall_std_dev,overall_variance,overall_mean_std,overall_std_std,overall_variance_std
 # Load data from CSV file
 data = np.genfromtxt('cardiovascular_data.csv', delimiter=';', skip_header=1)
 
 # Extracting data columns
-age = (data[:, 0] / 365).astype(float)
+age = data[:, 0].astype(int)
 gender = data[:, 1].astype(int)
 height = data[:, 2].astype(int)
 weight = data[:, 3].astype(float)
@@ -52,8 +76,9 @@ alco = data[:, 9].astype(int)
 active = data[:, 10].astype(int)
 cardio = data[:, 11].astype(int)
 
-ap_hi_novo = np.clip(ap_hi,0,500)
-ap_lo_novo = np.clip(ap_lo,0,500)
+ap_hi_novo = np.clip(ap_hi,0,300)
+ap_lo_novo = np.clip(ap_lo,0,300)
+
 
 # List of discrete variables and their names
 discrete_variables = [gender, cholesterol, gluc, smoke, alco, active, cardio]
@@ -65,11 +90,11 @@ for i, variable in enumerate(discrete_variables):
     probabilities = counts / len(variable)
     
     plt.figure()  # Create a new figure for each plot
-    plt.bar(unique_values, probabilities, label=f'{nome_discretas[i]} Distribution')
+    plt.bar(unique_values, probabilities, label=f' Distribuicao da variavel: {nome_discretas[i]}')
     plt.xticks(unique_values)
     plt.xlabel(nome_discretas[i])
-    plt.ylabel('Probability')
-    plt.title(f'{nome_discretas[i]} Distribution')
+    plt.ylabel('Probabilidade')
+    plt.title(f'Distribuicao da variavel: {nome_discretas[i]} ')
     plt.show()
     
     mean = np.mean(variable)
@@ -82,21 +107,17 @@ for i, variable in enumerate(discrete_variables):
     print('\n')
 
 # List of continuous variables and their names
-continuous_variables = [age, height, weight, ap_hi, ap_lo]
+continuous_variables = [age, height, weight, ap_hi_novo, ap_lo_novo]
 nome_continua = ['age', 'height', 'weight', 'ap_hi', 'ap_lo']
 
 # Display continuous variable histograms
 for i, variable in enumerate(continuous_variables):
     plt.figure()  # Create a new figure for each plot
-    if nome_continua[i] == 'ap_hi':
-        plt.hist(ap_hi_novo, bins=100, density=True, alpha=0.6, color='g')
-    elif nome_continua[i]=='ap_lo':
-        plt.hist(ap_lo_novo, bins=100, density=True, alpha=0.6, color='g')
-    else:
-        plt.hist(variable, bins=100, density=True, alpha=0.6, color='g')
+    
+    plt.hist(variable, bins=25, density=True, alpha=0.6, color='g')
     plt.xlabel(nome_continua[i])
-    plt.ylabel('Density')
-    plt.title(f'{nome_continua[i]} Histogram')
+    plt.ylabel('Densidade')
+    plt.title(f'Histograma da variavel: {nome_continua[i]}')
     plt.axvline(x=np.mean(variable), color='g', linestyle='--', label=f'Média: {np.mean(variable):.2f}')
     plt.show()
 
@@ -110,7 +131,7 @@ for i, variable in enumerate(continuous_variables):
     print('\n')
 
 data_file = 'cardiovascular_data.csv'
-means, std_devs, variances,means_geral,std_geral,var_geral = calculate_statistics(data_file,5)
+means, std_devs, variances,means_geral,std_geral,var_geral,means_std,std_std,ver = calculate_statistics(data_file,35)
 print("Media de cada amostra:")
 print(means)
 print("Desvio padrao de cada amostra:")
